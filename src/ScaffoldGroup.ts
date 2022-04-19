f4954ddc63d81fec50c09af2049f1d61e7637354
Jim Jeffers <jim@sumocreations.com>
2022-04-19 10:48:25 -0700

    Scaffolds now format output with prettier.

--- src/ScaffoldGroup.ts
+++ src/ScaffoldGroup.ts
@@ -1,67 +1,66 @@
-import Scaffold from "simple-scaffold";
-import path from "path";
-import { ScaffoldGroupCmdConfig } from "./types";
+import Scaffold from 'simple-scaffold'
+import path from 'path'
+import { ScaffoldGroupCmdConfig } from './types'
+import prettier from 'prettier'
 
 const deconstruct = (props?: string[]) =>
   (props ?? []).reduce((acc, prop) => {
-    const [name] = prop.split(":");
-    return `${acc}, ${name.replace(/[^\w\s]/gi, "")}`;
-  }, "");
+    const [name] = prop.split(':')
+    return `${acc}, ${name.replace(/[^\w\s]/gi, '')}`
+  }, '')
 
 const buildProps = (props?: string[]) =>
   (props ?? []).reduce((acc, prop, i) => {
-    const [name, type] = prop.split(":");
-    return `${acc}${name}: ${type}${
-      i < (props?.length ?? 0) - 1 ? "\n  " : ""
-    }`;
-  }, "");
+    const [name, type] = prop.split(':')
+    return `${acc}${name}: ${type}${i < (props?.length ?? 0) - 1 ? '\n  ' : ''}`
+  }, '')
 
 const argForType = (type: string) => {
   switch (type) {
-    case "string":
-      return '"example"';
-    case "number":
-      return "1";
-    case "boolean":
-      return "true";
-    case "object":
-      return "{}";
-    case "array":
-      return "[]";
-    case "block":
-      return "() => {}";
+    case 'string':
+      return '"example"'
+    case 'number':
+      return '1'
+    case 'boolean':
+      return 'true'
+    case 'object':
+      return '{}'
+    case 'array':
+      return '[]'
+    case 'block':
+      return '() => {}'
     default:
-      return "{} as any";
+      return '{} as any'
   }
-};
+}
 
 const buildArgs = (props?: string[]) =>
   (props ?? []).reduce((acc, prop, i) => {
-    const [name, type] = prop.split(":");
-    return `${acc}${name.replace(/[\W_]+/g, "")}: ${argForType(
-      type.indexOf("[") > -1
-        ? "array"
-        : type.indexOf("=>") > -1
-        ? "block"
+    const [name, type] = prop.split(':')
+    return `${acc}${name.replace(/[\W_]+/g, '')}: ${argForType(
+      type.indexOf('[') > -1
+        ? 'array'
+        : type.indexOf('=>') > -1
+        ? 'block'
         : type
-    )}${i < (props?.length ?? 0) ? ",\n  " : ""}`;
-  }, "");
+    )}${i < (props?.length ?? 0) ? ',\n  ' : ''}`
+  }, '')
 
 export async function ScaffoldGroup({
   name,
   ...options
 }: ScaffoldGroupCmdConfig) {
-  let scaffolds: Promise<void>[] = [];
+  let scaffolds: Promise<void>[] = []
 
-  let output = name.split("/");
-  const componentName = output.pop();
+  let output = name.split('/')
+  const componentName = output.pop()
 
   if (!componentName) {
-    throw new Error("No component name provided");
+    throw new Error('No component name provided')
   }
 
-  const template = String(options.template).toLowerCase();
-  const method = options.method ?? "post";
+  const template = String(options.template).toLowerCase()
+  const method = options.method ?? 'post'
 
   let data = {
     ...(options.data ?? {}),
@@ -69,29 +68,36 @@ export async function ScaffoldGroup({
     args: buildArgs(options.props),
     returnProps: buildProps(options.returnProps),
     deconstructedProps: deconstruct(options.props),
-    path: options.path ?? "",
+    path: options.path ?? '',
     method,
-    requestParams: ["get", "delete"].includes(method)
-      ? "`${url}?${new URLSearchParams({ ...params })}`"
-      : "url, params",
-  };
+    requestParams: ['get', 'delete'].includes(method)
+      ? '`${url}?${new URLSearchParams({ ...params })}`'
+      : 'url, params'
+  }
+
+  const prettierOptions = await prettier.resolveConfig(process.cwd())
 
   scaffolds.push(
     Scaffold({
       ...options,
       name: componentName,
       templates: [
-        path.join(__dirname, "../scaffolds", template, "*"),
-        path.join(__dirname, "../scaffolds", template, "**/*"),
+        path.join(__dirname, '../scaffolds', template, '*'),
+        path.join(__dirname, '../scaffolds', template, '**/*')
       ],
       data: {
         moduleName: output[output.length - 1],
-        ...data,
+        ...data
       },
       output: path.join(process.cwd(), ...output),
       createSubFolder: false,
+      beforeWrite: (content) =>
+        prettier.format(content.toString(), {
+          ...prettierOptions,
+          parser: 'babel-ts'
+        })
     })
-  );
+  )
 
-  return Promise.all(scaffolds);
+  return Promise.all(scaffolds)
 }
